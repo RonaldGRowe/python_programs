3e2e96a279b28db84ec82ef35c580a6264fec66c
Ronald Rowe <ronaldgilesrowe@gmail.com>
2022-06-14 15:40:26 -0500

    Update twosumsforloop.py

--- twosumsforloop.py
+++ twosumsforloop.py
@@ -1,6 +1,6 @@
 #!/bin/usr/env python3
 
-
+#If 2*n = target find the second matching value woould avoid issues
 #More Leetcode fun the obvious for loops
 #working on different method of traversing list
 def twosums(numslist):
